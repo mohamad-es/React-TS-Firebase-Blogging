d9458c4bc32736cbb7726965e24b4d5c74585ef8
Mohammad Esmaeilpour <imohmdsmp@gmail.com>
2025-01-07 15:27:51 +0330

    add new ui for profile

--- src/pages/Blogs/_components/EditFormSidebar.tsx
+++ src/pages/Blogs/_components/EditFormSidebar.tsx
@@ -16,6 +16,7 @@ const EditFormSidebar = ({ loading, modalsRef }: Props) => {
           onClick={() => modalsRef.current?.showModal()}
           className="flex items-center gap-2 border rounded-xl px-3 py-2 text-sm transition-all hover:bg-blue-600 hover:text-white"
         >
+          
           Preview
           <EyeIcon size={18} />
         </button>
